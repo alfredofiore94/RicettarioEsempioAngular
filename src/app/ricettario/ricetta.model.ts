d8c10096a98b33dc93228013cd03dd849bade9d5
alfredofiore <alfredofiore.work@gmail.com>
2021-10-14 12:46:42 +0200

    model esempio + lista ricette

---
+++ src/app/ricettario/ricetta.model.ts
@@ -0,0 +1,15 @@
+export class Ricetta {
+
+    public nome: String;
+    public descrizione: String;
+    public pathImg: String;
+
+    constructor(nome: String, descrizione: String, pathIm: string) {
+    
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.pathImg = pathIm;
+    }
+
+
+}
--- src/app/ricettario/ricettario-lista/ricettario-lista.component.ts
+++ src/app/ricettario/ricettario-lista/ricettario-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ricetta } from '../ricetta.model';
 
 @Component({
   selector: 'app-ricettario-lista',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ricettario-lista.component.css']
 })
 export class RicettarioListaComponent implements OnInit {
+
+  ricette: Ricetta[] = [new Ricetta(
+
+    'Test nome ricetta', 'test dec ricetta', 'https://www.mauriziotommasini.it/wp-content/uploads/2018/02/cibo_spazzatura_cancro_salute.jpg'
+  )
+  ];
 
   constructor() { }
 
